e5930eb2557f5b04f52e914efbc1324c8967afc2
Tomek Padzik <tomek.padzik@ingeniousio.com>
2024-03-03 22:27:49 +0200

    cleanup

--- app/Modules/UploadedImages/Http/Controllers/UploadedImagesController.php
+++ app/Modules/UploadedImages/Http/Controllers/UploadedImagesController.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace App\Modules\UploadedImages\Http\Controllers;
 
-use App\Modules\UploadedImages\Http\Requests\ImagesIndexRequest;
 use App\Modules\UploadedImages\Http\Requests\ImageUploadRequest;
 use App\Modules\UploadedImages\Repositories\Contracts\UploadedImagesRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Str;
@@ -32,13 +30,14 @@ readonly class UploadedImagesController
         }
 
         Session::remove('after_login_route');
-        
+
         return View::make(
-            'UploadedImages::index', ['uploaded' => $this->repository->all()->toArray()],
+            'UploadedImages::index',
+            ['uploaded' => $this->repository->all()->toArray()],
         );
     }
 
-    public function upload(ImageUploadRequest $request)
+    public function upload(ImageUploadRequest $request): JsonResponse
     {
         $file = $request->file('file');
         $filename = sprintf(
@@ -49,7 +48,7 @@ readonly class UploadedImagesController
 
         $file->move(public_path('images'), $filename);
 
-        $model = $this->repository->create(
+        $this->repository->create(
             name: $request->get('name'),
             surname: $request->get('surname'),
             image: $filename,
